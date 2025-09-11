f0f3e27b5c9322ff9cd04774d3b57ae295465f36
TheLeoP <eugenio2305@hotmail.com>
2025-09-11 13:37:36 -0500

    chore: add tanstack query's dev tools

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -12,5 +13,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 )
