a1e54e9ec6e100a19d9ff6f3bad4ba2ee1f2f8a4
TheLeoP <eugenio2305@hotmail.com>
2026-01-27 21:55:25 -0500

    fix: use old version of WhatsApp web to avoid breaking changes

--- src/main/index.ts
+++ src/main/index.ts
@@ -154,9 +154,10 @@ let authenticated = false
 let qr: string | null = null
 async function init(win: BrowserWindow) {
   const opts: ClientOptions = {
+    // HACK: remove after whatsapp-web.js is updated
     webVersionCache: {
-      type: 'local',
-      path: `${app.getPath('userData')}/.wwebjs_cache/`
+      type: 'remote',
+      remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/refs/heads/main/html/2.3000.1031490220-alpha.html`
     },
     authStrategy: new LocalAuth({
       dataPath: `${app.getPath('userData')}/.wwebjs_auth/`
